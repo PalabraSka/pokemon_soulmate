c3588b67c46b5da21fb1ab2cba2946944d89fef2
Ska <126164318+PalabraSka@users.noreply.github.com>
2025-09-10 13:55:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import * from Flask
+from dorenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_pokemon(pokemon="pikachu"):
+  request_url = f'https://pokeapi.co/api/v2/pokemon/' + pokemon
+  pokemon_data = requests.get(request_url).json()
+
+  return pokemon_data
+
