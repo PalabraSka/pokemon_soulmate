abc8e813714bde069f7097e9e5afe3c49f7412a6
Ska <126164318+PalabraSka@users.noreply.github.com>
2025-09-12 13:01:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ function render_encounters() {
   encounters_div.innerHTML = null;
 
   for (const [idx, item] of Object.entries(encounters)) {
+    const container = document.createElement("div")
+    container.style.marginBottom = "10px"
+    
     const text = document.createElement("p")
     text.textContent = item;
-    encounters_div.appendChild(text)
+
+    const button_del = document.createElement("button")
+    button_del.textContent = "delete"
+    button_del.onclick = () => remove_encounter(idx)
+
+    container.appendChild(text)
+    container.appendChild(button_del)
+    
+    encounters_div.appendChild(container)
   }
 }
 
@@ -21,7 +32,7 @@ function save_encounters() {}
 
 function add_encounter() {}
 
-function remove_encounter() {}
+function remove_encounter(idx) {}
 
 function reset_encouters() {}
 
