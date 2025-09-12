ff18f76b1ad89f8796ed05ae82df04b46042d60f
Ska <126164318+PalabraSka@users.noreply.github.com>
2025-09-12 11:43:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let encounters = ["ditto", "pikachu"];
+
+
+const encouters_div = document.getElementById("encouters")
+
+function load_encounters() {}
+
+function render_encounters() {
+  encouters_div.innerHTML = null;
+
+  for (const [idx, item] of Object.entries(items)) {
+    const text = document.createElement("p")
+    text.textContent = item;
+    encounters_div.appendChild(text)
+  }
+}
+
+render_encounters()
+
+function save_encounters() {}
+
+function add_encounter() {}
+
+function remove_encounter() {}
+
+function reset_encouters() {}
 
