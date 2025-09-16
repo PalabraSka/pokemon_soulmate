18ac796b7dce82dda92d0d70d51a19b10742da40
Ska <126164318+PalabraSka@users.noreply.github.com>
2025-09-16 21:07:21 +0200

    Update pokedex_builder.js

--- tools/pokedex_builder.js
+++ tools/pokedex_builder.js
@@ -2,27 +2,114 @@ const api_url = "https://pokeapi.co/api/v2/"
 const generation_url = "generation/"
 const generations = ["generation-i", "generation-ii", "generation-iii", "generation-iv", "generation-v", "generation-vi", "generation-vii", "generation-viii", "generation-ix"]
 
-const pokemon = {
-  sprite: "",
-  species: "",
-  type1: null,
-  type2: null
+const encounters_div = document.getElementById("encounters_div")
+
+function pokemon_(pk_name, pk_sprite, pk_species, pk_type1, pk_type2=null) {
+  this.name = pk_name
+  this.sprite: pk_sprite
+  this.species: pk_species
+  this.type1: pk_type1
+  this.type2: pk_type2
 }
 
 function render_page() {
   for (let i = 0; i < generation.length; i++) {
-    const text 
+    const container = document.createElement("div")
+    container.id = i
+    container.className = encounter_class
+    
+    const text = document.createElement("p")
+    input.id = i
+    input.value = generations[i]
+    input.style.display = "inline"
+    input.className = encounter_class
+
+    const button_create = document.createElement("button")
+    button_create.textContent = "edit"
+    button_create.onclick = () => build_pokedex(idx)
+    button_create.className = encounter_class
+    
+    container.appendChild(text)
+    container.appendChild(button_create)
+    
+    encounters_div.appendChild(container)
   }
 }
 
 function build_pokedex(generation) {
-    if (generation < 0 || generation >= generations.length) {
-        return
-    }
+  if (generation < 0 || generation >= generations.length) {
+    return
+  }
 
-    var pokedex = []
-    const nb_species = count_species(generation)
-    
+  var pokedex = []
+  const nb_species = count_species(generation)
+
+  fetch(api_url + "/pokemon?limit=" + nb_species.toString())
+    .then(response => response.json())
+    .then(function(allpokemon) {
+      allpokemon.results.forEach(function(pokemon) {
+        fetch_pokemon_data(pokemon, generation)
+      })
+    })
+  
+}
+
+function fetch_pokemon_data(pokemon, gen) {
+  let url = pokemon.url // <--- this is saving the pokemon url to a variable to use in the fetch. 
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData){
+        const new_name = pokeData.species.name
+        const new_sprite = pokeData.sprites.front_default
+        const new_species = get_first_evolution(pokeData)
+        const new_type1 = get_type_1(pokeData, gen)
+        const new_type2 = get_type_2(pokeData, gen)
+        const new_pokemon = new pokemon_(new_name, new_sprite, new_species, new_type1, new_type2)
+    })
+}
+
+function get_first_evolution(pokemon){
+  let species
+  
+  fetch(pokemon.species.url)
+    .then(response => response.json())
+    .then(function(pokeData){
+      if (!pokeData.evolution_chain.url) {
+        species = pokeData.name
+      } else {
+        fetch(pokeData.evolution_chain.url)
+        .then(response => response.json())
+        .then(function(pokeData2) {
+          species = pokeData2.chain.species.name
+        })
+      }
+    }
+  return species
+}
+
+function get_type_1(pokemon, gen) {
+  if (pokemon.past_types && is_this_gen_included(pokemon.past_types[0].generation.name, gen)) {
+    return pokemon.past_types[0].types[0].type.name
+  }
+  return pokemon.types[0].type.name
+}
+
+function get_type_2(pokemon, gen) {
+  if (pokemon.past_types && is_this_gen_included(pokemon.past_types[0].generation.name, gen)) {
+    if (!pokemon.past_types[0].types[1].type.name) {
+      return pokemon.past_types[0].types[1].type.name
+    }
+    return null
+  }
+  if (!pokemon.past_types[0].types[1].type.name) {
+    return pokemon.types[1].type.name
+  }
+  return null
+}
+
+function is_this_gen_included(text, generation) {
+  return true
 }
 
 function count_species(generation) {
